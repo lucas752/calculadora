b405b2d00c0f1abaf0d9319f6d9ece06ac01db06
Lucas Monteiro <77646992+lucas752@users.noreply.github.com>
2023-07-16 15:36:01 -0300

    refactor: reorganizando o layout da calculadora

--- src/components/Calculator/index.jsx
+++ src/components/Calculator/index.jsx
@@ -91,34 +91,34 @@ export default function Calculator() {
                     <Button text={'M-'} textColor={'text-blue-400'} handleClick={subMemory}/>
                 </div>
                 <div className="flex justify-between w-[100%]">
-                    <Button text={'+'} handleClick={addToExpression} textColor={'text-blue-400'}/>
+                    <Button text={'7'} handleClick={addToExpression}/>
+                    <Button text={'8'} handleClick={addToExpression}/>
+                    <Button text={'9'} handleClick={addToExpression}/>
+                    <Button text={'+'} handleClick={addToExpression} textColor={'text-blue-400'}/> 
+                </div>
+                <div className="flex justify-between w-[100%]">
+                    <Button text={'4'} handleClick={addToExpression}/>
+                    <Button text={'5'} handleClick={addToExpression}/>
+                    <Button text={'6'} handleClick={addToExpression}/>
                     <Button text={'-'} handleClick={addToExpression} textColor={'text-blue-400'}/>
-                    <Button text={'*'} handleClick={addToExpression} textColor={'text-blue-400'}/>
-                    <Button text={'/'} handleClick={addToExpression} textColor={'text-blue-400'}/>
                 </div>
                 <div className="flex justify-between w-[100%]">
                     <Button text={'1'} handleClick={addToExpression}/>
                     <Button text={'2'} handleClick={addToExpression}/>
                     <Button text={'3'} handleClick={addToExpression}/>
-                    <Button text={'4'} handleClick={addToExpression}/>
+                    <Button text={'*'} handleClick={addToExpression} textColor={'text-blue-400'}/>
                 </div>
                 <div className="flex justify-between w-[100%]">
-                    <Button text={'5'} handleClick={addToExpression}/>
-                    <Button text={'6'} handleClick={addToExpression}/>
-                    <Button text={'7'} handleClick={addToExpression}/>
-                    <Button text={'8'} handleClick={addToExpression}/>
-                </div>
-                <div className="flex justify-between w-[100%]">
-                    <Button text={'9'} handleClick={addToExpression}/>
                     <Button text={'0'} handleClick={addToExpression}/>
                     <Button text={'.'} handleClick={addToExpression}/>
-                    <Button text={'='} handleClick={calculateResult} backgroundColor={'bg-blue-400'}/>
+                    <Button text={<Backspace size={25} weight="bold" color="#f87171"/>} handleClick={deleteInput} />
+                    <Button text={'/'} handleClick={addToExpression} textColor={'text-blue-400'}/>
                 </div>
                 <div className="w-[100%] flex justify-between">
                     <div className="w-[3.8rem] h-[3.8rem]"></div>
                     <div className="w-[3.8rem] h-[3.8rem]"></div>
                     <Button text={'C'} handleClick={clearInput} textColor={'text-red-400'}/>
-                    <Button text={<Backspace size={25} weight="bold" color="#f87171"/>} handleClick={deleteInput} />
+                    <Button text={'='} handleClick={calculateResult} backgroundColor={'bg-blue-400'}/>
                 </div>
             </div>
       </div>
